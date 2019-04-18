8a1e3facb51d4009dea6ad619d56fdbde9c05397
TheKevSter-NL <43183768+TheKevSter-NL@users.noreply.github.com>
2019-04-18 22:32:22 +0200

    Add files via upload

---
+++ mininum.js
@@ -0,0 +1,9 @@
+//#1
+console.log("Exercise 4: mininum")
+function minimumHw4(number, number2) {
+
+  var hw4 = Math.min(number, number2); //starting with 1 #
+  console.log(hw4);
+}
+
+minimumHw4(2500, 5000);
---
+++ recursion.js
@@ -0,0 +1,22 @@
+console.log("Exercise 5: Recursion")
+function checkHw5(nummer){
+
+  if (nummer % 2 === 0){
+    return true;
+  }
+
+  else if (nummer < 0) {
+    return false;
+  }
+
+  else {
+    return false;
+  }
+
+}
+
+console.log(checkHw5(50));
+// → true
+console.log(checkHw5(75));
+// → false
+console.log(checkHw5(-1));
